1bf7263cc5fd76c3061b0f6286a0576149d50e5d
Revenant70 <jackmcg777@protonmail.com>
2024-04-24 00:36:30 -0400

    feat: add config inspection button which enlarges the card [ConfigLayout]

--- Frontend/src/components/ConfigLayout.jsx
+++ Frontend/src/components/ConfigLayout.jsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faClipboard } from "@fortawesome/free-regular-svg-icons";
+import { faClipboard, faPenToSquare } from "@fortawesome/free-regular-svg-icons";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 import { useEffect, useRef } from "react";
@@ -11,6 +12,8 @@ import { motion, AnimatePresence } from "framer-motion";
 
 export default function ConfigLayout() {
   const [createCardOpen, setCreateCardOpen] = useState(false);
+  const [editCardOpen, setEditCardOpen] = useState(false);
+
   const [configCards, setConfigCards] = useState([]);
   const [expandedIndex, setExpandedIndex] = useState(null);
   const [importCard, setImportCard] = useState(null);
@@ -36,6 +39,7 @@ export default function ConfigLayout() {
     },
   }
 
+
   const variants = {
     visible: i => ({
       opacity: 1,
@@ -45,6 +49,10 @@ export default function ConfigLayout() {
     }),
     hidden: { opacity: 0 },
   }
+
+  const onCardClick = (index) => {
+    setExpandedIndex(index === expandedIndex ? null : index);
+  };
 
   const fetchConfigCards = async () => {
     try {
@@ -102,46 +110,61 @@ export default function ConfigLayout() {
   return (
     <>
       <motion.div
-        className="grid grid-cols-3 gap-2 w-full"
+        className={`grid grid-cols-3 gap-2 w-full h-full relative`}
         variants={list}
         initial="hidden"
         animate="visible"
         
       >
+        {expandedIndex !== null && (
+        <div className="bg-black opacity-50 w-full h-full absolute z-30"></div>
+          )}
         {configCards.map((configCard, index) => (
           <motion.div
             layout 
             key={index.id}
             custom={index}
-            initial="hidden"
+            initial={"hidden"}
             animate={"visible"}
             exit="hidden"
             variants={variants}
-            className={`p-4 rounded-md cursor-pointer`}
+            className={`p-4 rounded-md ${expandedIndex === index ? 'absolute inset-0 m-auto left-0 right-0 top-0 bottom-0 z-40' : ''}`}
+            transition={{ duration: 0.3 }}
           >
-            <pre
+
+            <motion.pre
               ref={codeBlockRef}
-              className="bg-[#202127] p-4 rounded-xl overflow-hidden h-[44vh]"
+              className="bg-[#202127] p-4 rounded-xl overflow-hidden h-[44vh] relative"
+
             >
               <div className="flex flex-row justify-between">
-                <div className="bg-[#32363F] rounded-lg w-9 h-9 flex justify-center items-center text-[#B892FF]">
-                  <motion.button
-                    className=""
-                    onClick={() => copyCodeToClipboard(configCard.content)}
-                    whileTap={{ scale: 0.85 }}
-                  >
+                <motion.button className="bg-[#32363F] rounded-lg w-9 h-9 flex justify-center items-center text-[#B892FF]"
+                onClick={() => copyCodeToClipboard(configCard.content)}
+                whileTap={{ scale: 0.85 }}
+                >
                     <FontAwesomeIcon icon={faClipboard} size="lg" />
-                  </motion.button>
-                </div>
+                </motion.button>
                 <div className="text-lg text-[#B892FF] rounded-lg h-9 flex justify-end p-2 items-center bg-[#32363F]">
                   {configCard.name}
                 </div>
               </div>
-
               <code className="text-sm text-[#deded6] font-mono ">
                 {configCard.content}
               </code>
-            </pre>
+              <motion.button className="absolute left-4 bottom-4 text-[#B892FF] bg-[#32363F] rounded-md h-9 w-9 flex justify-center items-center"
+                whileTap={{ scale: 0.85 }}
+                onClick={() => onCardClick(index)}
+              >
+                <FontAwesomeIcon icon={faPenToSquare}  size="md"/>
+              </motion.button>
+              <motion.button
+              className="absolute right-4 bottom-4 text-[#B892FF] bg-[#32363F] rounded-md h-9 w-9 flex justify-center items-center"
+              whileTap={{ scale: 0.85 }}
+              onClick={() => onCardClick(index)}
+              >
+                  <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+              </motion.button>
+            </motion.pre>
           </motion.div>
         ))}
       </motion.div>
--- Frontend/src/components/ConfigPage.jsx
+++ Frontend/src/components/ConfigPage.jsx
@@ -6,7 +6,7 @@ export default function Configs() {
     <>
   <div className="flex flex-row justify-end bg-[#1b1b1f] min-h-screen">
     <Sidebar />
-    <div className="flex flex-col overflow-x-auto w-10/12 p-4">
+    <div className="flex flex-col overflow-x-auto w-10/12">
       <div className="flex justify-end items-start flex-grow">
           <ConfigLayout />
       </div>
